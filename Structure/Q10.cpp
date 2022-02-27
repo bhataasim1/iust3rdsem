94072e13afca79e96a434611f7d547e5e70a5fbd
Bhat Aasim <71691906+bhataasim1@users.noreply.github.com>
2022-02-27 11:55:52 +0530

    Create Q10.cpp

---
+++ Structure/Q10.cpp
@@ -0,0 +1,8 @@
+/*
+Create a structure called sterling that stores money amounts in the old-style British system discussed in Exercises 8 and 11 in Chapter 3, “Loops and Decisions.” 
+The members could be called pounds , shillings , and pence , all of type int . 
+The program should ask the user to enter a money amount in new-style decimal pounds (type double ), convert it to the old-style system, 
+store it in a variable of type struct sterling , and then display this amount in pounds-shillings-pence format.
+*/
+
+
