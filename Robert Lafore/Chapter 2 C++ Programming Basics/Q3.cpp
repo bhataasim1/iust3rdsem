3ad753299869ad802255220fd59cb5a64f300cf4
Bhat Aasim <71691906+bhataasim1@users.noreply.github.com>
2022-03-05 12:47:30 +0530

    Create Q3.cpp

---
+++ Robert Lafore/Chapter 2 C++ Programming Basics/Q3.cpp
@@ -0,0 +1,17 @@
+/*
+Write a program that generates the following output:
+10
+20
+19
+*/
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    cout << "10" << endl << "20" << endl <<"19";
+    return 0;
+}
