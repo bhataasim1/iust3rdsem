71d039527699f42915ecbe6badd9237a9cd5ef3d
Bhat Aasim <71691906+bhataasim1@users.noreply.github.com>
2022-05-02 15:59:33 +0530

    Update 1.cpp

--- Robert Lafore/Chapter 6 : Classes & Objects/1.cpp
+++ Robert Lafore/Chapter 6 : Classes & Objects/1.cpp
@@ -2,6 +2,9 @@
 Create a class that imitates part of the functionality of the basic data type int. Call the
 class Int (note different capitalization). The only data in this class is an int variable.
 Include member functions to initialize an Int to 0, to initialize it to an int value, to display it (it looks just like an int), and to add two Int values.
+Write a program that exercises this class by creating one uninitialized and two initialized
+Int values, adding the two initialized values and placing the response in the uninitialized
+value, and then displaying this result.
 */
 
 
